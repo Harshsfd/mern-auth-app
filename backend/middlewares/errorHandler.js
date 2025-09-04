7e78cc8aaeb3e859809afd8b87799daab39b94b4
Harsh Bhardwaj <89222581+Harshsfd@users.noreply.github.com>
2025-09-05 00:53:50 +0530

    Update errorHandler.js

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,8 +1,4 @@
-const errorHandler = (err, req, res, next) => {
+export const errorHandler = (err, req, res, next) => {
   console.error(err.stack);
-  res.status(err.statusCode || 500).json({
-    message: err.message || 'Server Error'
-  });
+  res.status(500).json({ message: err.message });
 };
-
-module.exports = errorHandler;
