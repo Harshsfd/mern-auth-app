9fa6640f65ed09e945ab001a36d9a70dfd2b674e
Harsh Bhardwaj <89222581+Harshsfd@users.noreply.github.com>
2025-09-05 01:53:17 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 const sendEmail = async (to, subject, html) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail", // Gmail ke liye
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
