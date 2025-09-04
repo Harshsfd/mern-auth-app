e36731e861afdd9a8f37a1792657cebf1f027147
Harsh Bhardwaj <89222581+Harshsfd@users.noreply.github.com>
2025-09-05 01:52:26 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,20 +5,22 @@ const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    mobile: { type: String },
     password: { type: String, required: true },
-    resetToken: { type: String },
-    resetTokenExpiry: { type: Date },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
   },
   { timestamps: true }
 );
 
+// Encrypt password before save
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
+// Match password
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
