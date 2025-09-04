82a10b26930508dd4e02de9c008af92d2b97ba38
Harsh Bhardwaj <89222581+Harshsfd@users.noreply.github.com>
2025-09-05 01:53:53 +0530

    Update errorHandler.js

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,4 +1,9 @@
-export const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: err.message });
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode).json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
 };
+
+export default errorHandler;
