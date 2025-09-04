e481203ced6fc305319027545fd79b180565f62a
Harsh Bhardwaj <89222581+Harshsfd@users.noreply.github.com>
2025-09-05 02:29:55 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,21 +1,3 @@
-// @desc    Get user profile
-export const getUserProfile = async (req, res) => {
+export const getProfile = async (req, res) => {
   res.json(req.user);
-};
-
-// @desc    Update user profile
-export const updateUserProfile = async (req, res) => {
-  try {
-    req.user.name = req.body.name || req.user.name;
-    req.user.email = req.body.email || req.user.email;
-    if (req.body.password) req.user.password = req.body.password;
-    const updatedUser = await req.user.save();
-    res.json({
-      _id: updatedUser._id,
-      name: updatedUser.name,
-      email: updatedUser.email,
-    });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
 };
