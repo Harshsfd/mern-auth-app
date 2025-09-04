2a3df66051828e0a9b2ad077b0a95f2483fb23a6
Harsh Bhardwaj <89222581+Harshsfd@users.noreply.github.com>
2025-09-05 01:51:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,31 +5,26 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
-// Load environment variables
+// Load env vars
 dotenv.config();
 
-// Connect to MongoDB
+// Connect to Mongo
 connectDB();
 
 const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json()); // to parse JSON bodies
+app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
-// Default Route
+// Default route
 app.get("/", (req, res) => {
   res.send("MERN Auth Backend is running 🚀");
 });
 
-// PORT from .env or default 5000
 const PORT = process.env.PORT || 5000;
-
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
